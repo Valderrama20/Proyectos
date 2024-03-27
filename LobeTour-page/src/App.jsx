59b1c86ea6d90562bc028a643168cc3700c2154c
Valderrama20 <jose23122009@gmai.com>
2024-03-27 21:50:36 +0100

    trabajando en home

--- LobeTour-page/src/App.jsx
+++ LobeTour-page/src/App.jsx
@@ -1,12 +1,25 @@
 
 import style from './App.module.css'
 import Nav from './components/nav/Nav'
-
+import  video1 from './assets/video1.mp4'
 function App() {
   return (
     <div className={style.father}>
     <Nav/>
       
+     <div className={style.containerInfo}>
+      <div>
+        <span>Train apps to</span>
+      <div></div>
+      <span>Lobe helps you train machine learning models with a free, easy to use tool.</span>
+       <div>
+        <button>Download</button>
+        <button>What Tour</button>
+       </div>
+      </div>
+      <video src={video1} autoPlay loop></video>
+     </div>
+
     </div>
   )
 }
